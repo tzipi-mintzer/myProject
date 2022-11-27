0e9629b62d9d0ed541c06db2e1007ffd517aa889
tzipi-mintzer <m0527145332@gmail.com>
2022-11-27 20:58:15 +0200

    async 2

---
+++ MyProject.webAPI/Models/ClaimModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProject.webAPI.Models
+{
+    public enum EPolicy { Allow, Deny}
+
+    public class ClaimModel
+    {
+        public int Id { get; set; }
+
+        public int RoleId { get; set; }
+
+        public int PermissionId { get; set; }
+
+        public EPolicy Policy { get; set; }
+    }
+}
